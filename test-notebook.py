95cd62011057c16d647b203373e35508aa54211f
ayeo144@googlemail.com <ayeo144@googlemail.com>
2022-02-01 21:42:39 +0000

    Testing 'magic commands'

--- test-notebook.py
+++ test-notebook.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
 print('hello cluster')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello"
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+
